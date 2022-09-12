0a041410d249b5c1dca0e2d8d60cafb62de8d96b
3n3a <info@3n3a.ch>
2022-09-12 13:26:13 +0200

    add input strength meter

---
+++ exercises/a03/my-project/src/components/app.js
@@ -0,0 +1,20 @@
+import { h } from 'preact';
+import { Router } from 'preact-router';
+
+import Header from './header';
+
+// Code-splitting is automated for `routes` directory
+import Home from '../routes/home';
+import Login from '../routes/login';
+
+const App = () => (
+	<div id="app">
+		<Header />
+		<Router>
+			<Home path="/" />
+			<Login path="/login" />
+		</Router>
+	</div>
+)
+
+export default App;
