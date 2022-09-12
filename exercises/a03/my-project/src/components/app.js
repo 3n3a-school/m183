b368b12fb2c1826f8871eb04e88cc677ca762ca9
3n3a <info@3n3a.ch>
2022-09-12 14:15:19 +0200

    only use login

--- exercises/a03/my-project/src/components/app.js
+++ exercises/a03/my-project/src/components/app.js
@@ -1,22 +1,18 @@
 import { h } from 'preact';
 import { Router } from 'preact-router';
-import { createBrowserHistory } from 'history';
 
-const history = createBrowserHistory({ basename: '/m183/exercises/a03' });
 
 
 import Header from './header';
 
 // Code-splitting is automated for `routes` directory
-import Home from '../routes/home';
 import Login from '../routes/login';
 
 const App = () => (
 	<div id="app">
 		<Header />
-		<Router history={history}>
-			<Home path="/" />
-			<Login path="/login" />
+		<Router>
+			<Login path="/" />
 		</Router>
 	</div>
 )
