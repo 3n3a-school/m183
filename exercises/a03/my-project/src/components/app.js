dc45ae93fdbf0e4a7acabb37a343fdd888d37807
3n3a <info@3n3a.ch>
2022-09-12 14:08:39 +0200

    configure different history for preact-router

--- exercises/a03/my-project/src/components/app.js
+++ exercises/a03/my-project/src/components/app.js
@@ -1,5 +1,9 @@
 import { h } from 'preact';
 import { Router } from 'preact-router';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory({ basename: '/m183/exercises/a03' });
+
 
 import Header from './header';
 
@@ -10,9 +14,9 @@ import Login from '../routes/login';
 const App = () => (
 	<div id="app">
 		<Header />
-		<Router>
+		<Router history={history}>
 			<Home path="/" />
-			<Login path="login" />
+			<Login path="/login" />
 		</Router>
 	</div>
 )
