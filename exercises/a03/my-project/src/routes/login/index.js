e6856c009148bc01058967425fcb045a5f000ac2
3n3a <46775561+3n3a@users.noreply.github.com>
2022-09-13 17:49:48 +0200

    add debugging section

--- exercises/a03/my-project/src/routes/login/index.js
+++ exercises/a03/my-project/src/routes/login/index.js
@@ -10,38 +10,43 @@ import JSONPretty from 'react-json-pretty';
 import CryptoJS from 'crypto-js';
 
 const options = {
-  translations: zxcvbnEnPackage.translations,
-  graphs: zxcvbnCommonPackage.adjacencyGraphs,
-  dictionary: {
-    ...zxcvbnCommonPackage.dictionary,
-    ...zxcvbnEnPackage.dictionary,
-  },
+	translations: zxcvbnEnPackage.translations,
+	graphs: zxcvbnCommonPackage.adjacencyGraphs,
+	dictionary: {
+		...zxcvbnCommonPackage.dictionary,
+		...zxcvbnEnPackage.dictionary,
+	},
 }
 
 zxcvbnOptions.setOptions(options)
 
 class LoginForm extends Component {
-	state = { value: '', strength: {}, hash: {} };
-  
+	state = { value: '', strength: {}, hash: {}, isSubmitDisabled: true };
+
 	onSubmit = e => {
-	  e.preventDefault();
-          this.calcHash(this.state.value);
+		e.preventDefault();
+		this.calcHash(this.state.value);
 	}
-  
+
 	onInput = e => {
-	  const { value } = e.target;
-	  const strength = zxcvbn(value);
-	  
-	  this.setState({ value, strength })
+		const { value } = e.target;
+		const strength = zxcvbn(value);
+
+		let isSubmitDisabled = true
+		if (value.length >= 12) {
+			isSubmitDisabled = false
+		}
+
+		this.setState({ value, strength, isSubmitDisabled })
 	}
-  
+
 	calcHash = async password => {
 		const salt = CryptoJS.lib.WordArray.random(256 / 8);
 		const hashSalt = CryptoJS.enc.Hex.stringify(salt);
 		// eslint-disable-next-line no-undef
 		argon2.hash(
-			{ 
-				pass: password, 
+			{
+				pass: password,
 				salt: hashSalt,
 				time: 11,
 				mem: 4096
@@ -57,46 +62,55 @@ class LoginForm extends Component {
 			})
 	}
 
-        verifyHash = async () => {
-          // eslint-disable-next-line no-undef
-          argon2.verify({ pass: this.state.value, encoded: this.state.hash.encoded })
-            .then(() => {
-              alert('Hash Verification Passed ✅')
-              console.log('OK')
-            })
-            .catch(e => {
-              alert(`${e.code}: ${e.message}`)
-              console.error(e.message, e.code)
-            })
-        }
-	render(_, { value, strength, hash }) {
-	  return (
-		<div class={style.twocols}>
-                  <div>
-			<form onSubmit={this.onSubmit} class={style.login}>
-			<div class={style.field}>
-				<label for="password">Passwort</label>
-				<input id="password" type="search" autoComplete='off' minLength={12} value={value} onChange={this.onInput} />
-				<meter value={strength.score} max="4"> {strength.score}/4 </meter> 
-				{
-					strength.feedback &&
-					<>
-						<p class={style.warning}>{strength.feedback.warning}</p>
-						<p class={style.help}>{strength.feedback.suggestions}</p>
-					</>
-				}
-                                { hash.encoded && <p>Hash: {hash.encoded}</p> }
-			</div>
-			<button type="submit">Submit</button>
-			</form>
-                        <button onClick={this.verifyHash}>Verify Hash</button>
-                    </div>
-			<JSONPretty class={style.login} id="json-pretty" data={strength} />
-			<JSONPretty class={style.login} id="json-pretty" data={hash} />
-		</div>
-	  );
+	verifyHash = async () => {
+		if (!this.state.value || !this.state.hash.hasOwnProperty('encoded')) return
+		// eslint-disable-next-line no-undef
+		argon2.verify({ pass: this.state.value, encoded: this.state.hash.encoded })
+			.then(() => {
+				alert('Hash Verification Passed ✅')
+				console.log('OK')
+			})
+			.catch(e => {
+				alert(`${e.code}: ${e.message}`)
+				console.error(e.message, e.code)
+			})
 	}
-  }
-  
+
+	render(_, { value, strength, hash, isSubmitDisabled }) {
+		return (
+			<div class={style.twocols}>
+				<div>
+					<form onSubmit={this.onSubmit} class={style.login}>
+						<div class={style.field}>
+							<label for="password">Passwort</label>
+							<input id="password" type="search" autoComplete='off' minLength={12} value={value} onChange={this.onInput} />
+							<meter value={strength.score} max="4"> {strength.score}/4 </meter>
+							{
+								(strength.feedback && value) &&
+								<>
+									<p class={style.warning}>{strength.feedback.warning}</p>
+									<p class={style.help}>{strength.feedback.suggestions}</p>
+								</>
+							}
+						</div>
+						{(hash.encoded && value) && <p class={style.hashResult}>Hash: {hash.encoded}</p>}
+						<button class={style.button} disabled={isSubmitDisabled} type="submit">Submit</button>
+					</form>
+					<div class={style.login}>
+						<button class={style.button} onClick={this.verifyHash}>Verify Hash</button>
+					</div>
+				</div>
+				<details class={style.debugging}>
+					<summary>
+						Debugging Output
+					</summary>
+					<JSONPretty class={style.login} id="json-pretty" data={strength} />
+					<JSONPretty class={style.login} id="json-pretty" data={hash} />
+				</details>
+			</div>
+		);
+	}
+}
+
 
 export default LoginForm;
